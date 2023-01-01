5b18de634b03cbbd29879756d9e7af1a0f2debae
Sebastian <89323242+sebastianelbacho@users.noreply.github.com>
2023-01-01 15:18:34 +0000

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,1 +1,31 @@
+const guitars = ['gibson', 'fender', 'gretsch', 'prs', 'ibanez']
+for(let i = 0; i < guitars.length; i++) {
+    console.log(guitars[i])
+}
+const cities = ['new york', 'los angeles', 'tokyo', 'lisbon', 'moscow', 'warsaw']
+for (let i = 0; i < cities.length; i++) {
+  console.log('The name of the city is: ${cities[i].tuUpperCase()}')
+}
 
+let i = 10
+while(i < 50) {
+    console.log(i)
+    i++;
+
+}
+
+
+let i = 1 
+do {
+    i++
+    console.log(i)
+} while(i < 4)
+  
+  
+  const numbers = [1, 2, 3, 4, 5]
+for(const number of numbers) {
+    console.log(number * 3);
+}
+  
+  
+  
