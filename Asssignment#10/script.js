e45cb3ee4d473d89aca4684fac089ded53a4d536
Sebastian <89323242+sebastianelbacho@users.noreply.github.com>
2023-01-02 20:15:00 +0000

    Create script.js

---
+++ Asssignment#10/script.js
@@ -0,0 +1,8 @@
+var webPeg = window.prompt("You are on one of mine assignments, type in Your name to continue");
+
+while(webPeg == "") {
+    webPeg = window.prompt("You are on one of mine assignments, type in your name to continue");
+
+    console.log("Thank You! Now you can see the full picture.");
+
+}
