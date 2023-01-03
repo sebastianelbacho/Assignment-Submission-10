95d1569f3ddb144f80606fcb1fa95b399e2c26b8
Sebastian <89323242+sebastianelbacho@users.noreply.github.com>
2023-01-03 15:23:42 +0000

    Create script.js

---
+++ while1.1/script.js
@@ -0,0 +1,16 @@
+var webPeg = window.prompt("You are on one of mine assignments, type in Your name to continue");
+
+while(webPeg == ""){
+    webPeg = window.prompt("Type in here please");
+}
+  console.log("thank You");
+
+
+// let text = "";
+// let i = 0;
+// while (i < 10) {
+//     text += "<br>The number is " + i;
+//     i++;
+// }
+
+// document.getElementById("demo").innerHTML = text;
